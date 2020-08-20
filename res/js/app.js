3b56e03780439881109b6e523c6fdb77eb3c73bc
iamratul <ratulsikder104@gmail.com>
2020-08-20 13:51:35 +0600

    Header section

---
+++ res/js/app.js
@@ -0,0 +1,12 @@
+// $(document).ready(() => {
+//   $(".default-option").click(function () {
+//     $(this).parent().toggleClass("active");
+//   });
+
+//   $(".select-ul li").click(function () {
+//     let curr = $(this).html();
+//     $(".default-option li").html(curr);
+//     $(this).parents(".select-wrapper").removeClass("active");
+//   });
+// });
+https://docs.google.com/forms/d/16sl-m6UKGrWlWzPkCzM-59TtR8MTR_Qr-P97P_8p7ZQ
