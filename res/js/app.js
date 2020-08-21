f45eeb3358a86e936ed848aedc27add6fbb02ec8
iamratul <ratulsikder104@gmail.com>
2020-08-21 10:39:22 +0600

    Form functionality-sliding

--- res/js/app.js
+++ res/js/app.js
@@ -28,7 +28,10 @@ function showTab(n) {
 }
 
 function nextPrev(n) {
-  // Increase or decrease the current tab by 1:
+  let modal = document.getElementById("myModal");
+  modal.style.display = "none";
+  tab[currentTab].style.display = "none";
+
   currentTab = currentTab + n;
   // if you have reached the end of the form...
   if (currentTab >= tab.length) {
@@ -43,50 +46,15 @@ function renderConfirm() {
   let editBtn = document.getElementById("editBtn");
   let modal = document.getElementById("myModal");
 
-  //   Node Renderning
-  confirmCheck.innerHTML = "";
+  // Render first form
   if (currentTab == 0) {
-    let age = document.querySelector("#age");
-    let temp = document.querySelector("#temp");
-    let sex = document.querySelector("#sex");
-
-    let li = document.createElement("li");
-    let attr = document.createElement("p");
-    let val = document.createElement("p");
-
-    // render age
-    attr.innerText = age.name;
-    val.innerText = age.value;
-    li.appendChild(attr);
-    li.appendChild(val);
-    confirmCheck.appendChild(li);
-
-    // render sex
-    li = document.createElement("li");
-    attr = document.createElement("p");
-    val = document.createElement("p");
-
-    attr.innerText = sex.name;
-    val.innerText = sex.options[sex.selectedIndex].value;
-    li.appendChild(attr);
-    li.appendChild(val);
-    confirmCheck.appendChild(li);
-
-    //render temparature
-    li = document.createElement("li");
-    attr = document.createElement("p");
-    val = document.createElement("p");
-
-    attr.innerText = temp.name;
-    val.innerText = temp.value;
-    li.appendChild(attr);
-    li.appendChild(val);
-    confirmCheck.appendChild(li);
+    firstForm();
+  } else if (currentTab == 1) {
+    renderCheckbox("symptoms");
+  } else {
+    renderCheckbox("ad-symptoms");
   }
-
-  //   final render
-  modal.style.display = "block";
-  confirmCheck.style.display = "inline-block";
+  // render second form
 
   //   Modal close handle
   editBtn.onclick = () => {
@@ -98,4 +66,75 @@ function renderConfirm() {
       modal.style.display = "none";
     }
   };
+}
+
+function firstForm() {
+  //   Node Renderning
+  let editBtn = document.getElementById("editBtn");
+  let modal = document.getElementById("myModal");
+  confirmCheck.innerHTML = "";
+
+  let age = document.querySelector("#age");
+  let temp = document.querySelector("#temp");
+  let sex = document.querySelector("#sex");
+  let scale = document.querySelector("input[type=radio]:checked");
+
+  let li = document.createElement("li");
+  let attr = document.createElement("p");
+  let val = document.createElement("p");
+
+  // render age
+  attr.innerText = age.name;
+  val.innerText = age.value;
+  li.appendChild(attr);
+  li.appendChild(val);
+  confirmCheck.appendChild(li);
+
+  // render sex
+  li = document.createElement("li");
+  attr = document.createElement("p");
+  val = document.createElement("p");
+
+  attr.innerText = sex.name;
+  val.innerText = sex.options[sex.selectedIndex].value;
+  li.appendChild(attr);
+  li.appendChild(val);
+  confirmCheck.appendChild(li);
+
+  //render temparature
+  li = document.createElement("li");
+  attr = document.createElement("p");
+  val = document.createElement("p");
+
+  attr.innerText = temp.name;
+  val.innerText = temp.value + "°" + scale.value;
+  li.appendChild(attr);
+  li.appendChild(val);
+  confirmCheck.appendChild(li);
+
+  //   final render
+  modal.style.display = "block";
+}
+
+// Render checkbox input
+function renderCheckbox(name) {
+  let editBtn = document.getElementById("editBtn");
+  let modal = document.getElementById("myModal");
+
+  confirmCheck.innerHTML = "";
+  let checkList = document.querySelectorAll(`input[name=${name}]:checked`);
+  let len = checkList.length;
+
+  confirmCheck.innerHTML = "<h3>Selected health problems you feel.</h3>";
+  for (let i = 0; i < len; i++) {
+    let li = document.createElement("li");
+    let attr = document.createElement("p");
+    let val = document.createElement("p");
+
+    attr.innerText = i + 1 + " " + checkList[i].value;
+    li.appendChild(attr);
+    confirmCheck.appendChild(li);
+  }
+
+  modal.style.display = "block";
 }
